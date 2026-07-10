0fa1f58f98a12086aee8989d7387c3a98ab2be34
Kevin-AC <kevin.argumedo@gmail.com>
2026-07-09 22:29:07 -0500

    Feat: sidebard nav, mobile, tablet, desktop view

--- personal-finance-app/starter-code/src/components/Sidebard.jsx
+++ personal-finance-app/starter-code/src/components/Sidebard.jsx
@@ -3,52 +3,52 @@ import { useState } from "react"
 export default function Sidebard(){
     const [isMinimized, setIsMinimized]= useState(false)
     return(
-        <aside className={`h-full rounded-r-2xl bg-grey-900 flex flex-col justify-between transition-all duration-300 ${isMinimized ? "w-28" : "w-75"}`}>
-            <div className="">
-                <div className="px-500 py-400">
+        <aside className={`p-0 bottom-0 absolute z-50 w-full  rounded-t-2xl  lg:h-full lg:rounded-t-none  lg:rounded-r-2xl bg-grey-900 flex flex-col justify-between transition-all duration-300 ${isMinimized ? "lg:w-28" : "lg:w-75"}`}>
+            <div className="h-full">
+                <div className="hidden lg:block px-500 py-400">
                     {isMinimized ? (
                         <img className="w-6" src="/images/logo-small.svg" alt="logo-small" />
                     ) : (
                         <img src="/images/logo-large.svg" alt="finance-logo" />
                     )}
                 </div>
-                <nav className="pr-300 ">
-                    <ul>
-                        <li className="px-400 py-200 rounded-r-xl border-l-4 border-green  bg-beige-100 ">
-                            <a href="#dashboard" className="flex gap-200 text-grey-900 preset-3 items-center">
-                                <img  src="/images/icon-nav-overview.svg" alt="icon-nav-overview" aria-hidden="true" />
-                                {!isMinimized && <span>Overview</span>}
+                <nav className="px-200 pt-150 lg:pr-300 lg:px-0 h-1/2">
+                    <ul className="flex justify-between items-center  lg:items-start lg:flex-col h-full ">
+                        <li className="pt-100 lg:px-400 selecctedNav">
+                            <a href="#dashboard" className="flex sm:flex-col lg:flex-row gap-200 text-grey-900 preset-3 items-center">
+                                <img className="w-[24px] h-[24px]"  src="/images/icon-nav-overview.svg" alt="icon-nav-overview" aria-hidden="true" />
+                                {!isMinimized && <span className="hidden sm:block">Overview</span>}
                             </a>
                         </li>
-                        <li className="px-400 py-200 rounded-r-xl ">
-                            <a href="#Transactions" className="flex gap-200 text-grey-300 preset-3 items-center">
-                                <img src="/images/icon-nav-transactions.svg" alt="icon-nav-overview" aria-hidden="true" />
-                                {!isMinimized && <span>Transactions</span>}
+                        <li className="pt-100 lg:px-400 ">
+                            <a href="#Transactions" className="flex sm:flex-col lg:flex-row gap-200 text-grey-300 preset-3 items-center">
+                                <img className="w-[24px] h-[24px]" src="/images/icon-nav-transactions.svg" alt="icon-nav-overview" aria-hidden="true" />
+                                {!isMinimized && <span className="hidden sm:block">Transactions</span>}
                             </a>
                         </li>
-                        <li className="px-400 py-200 rounded-r-xl ">
-                            <a href="#Budgets" className="flex gap-200 text-grey-300 preset-3 items-center">
-                                <img src="/images/icon-nav-budgets.svg" alt="icon-nav-overview" aria-hidden="true" />
-                                {!isMinimized && <span>Budgets</span>}
+                        <li className="pt-100 lg:px-400 ">
+                            <a href="#Budgets" className="flex sm:flex-col lg:flex-row gap-200 text-grey-300 preset-3 items-center">
+                                <img className="w-[24px] h-[24px]" src="/images/icon-nav-budgets.svg" alt="icon-nav-overview" aria-hidden="true" />
+                                {!isMinimized && <span className="hidden sm:block">Budgets</span>}
                             </a>
                         </li>
-                        <li className="px-400 py-200 rounded-r-xl ">
-                            <a href="#Pots" className="flex gap-200 text-grey-300 preset-3 items-center">
-                                <img src="/images/icon-nav-pots.svg" alt="icon-nav-overview" aria-hidden="true" />
-                                {!isMinimized && <span>Pots</span>}
+                        <li className="pt-100 lg:px-400 ">
+                            <a href="#Pots" className="flex sm:flex-col lg:flex-row gap-200 text-grey-300 preset-3 items-center">
+                                <img className="w-[24px] h-[24px]" src="/images/icon-nav-pots.svg" alt="icon-nav-overview" aria-hidden="true" />
+                                {!isMinimized && <span className="hidden sm:block">Pots</span>}
                             </a>
                         </li>
-                        <li className="px-400 py-200 rounded-r-xl ">
-                            <a href="#Recurring-Bills" className="flex gap-200 text-grey-300 preset-3 items-center">
-                                <img src="/images/icon-nav-recurring-bills.svg" alt="icon-nav-overview" aria-hidden="true" />
-                                {!isMinimized && <span>Recurring Bills</span>}
+                        <li className="pt-100 lg:px-400 ">
+                            <a href="#Recurring-Bills" className="flex sm:flex-col lg:flex-row gap-200 text-grey-300 preset-3 items-center">
+                                <img className="w-[24px] h-[24px]" src="/images/icon-nav-recurring-bills.svg" alt="icon-nav-overview" aria-hidden="true" />
+                                {!isMinimized && <span className="hidden sm:block">Recurring Bills</span>}
                             </a>
                         </li>
                     </ul>
                     
                 </nav>
             </div>
-            <button className="flex items-center gap-200 px-400 py-300 mb-400 w-full text-left cursor-pointer"
+            <button className="hidden  lg:flex items-center gap-200 px-400 py-300 mb-400 w-full text-left cursor-pointer"
                 onClick={() => setIsMinimized(!isMinimized)}
             >
                 <img className={`transition-transform duration-300 ${isMinimized ? "rotate-180 mx-auto" : ""}`} src="/images/icon-minimize-menu.svg" alt="icon-minimize-menu" />
