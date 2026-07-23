3b084b1f27dd90bbe1cf3a994aea61efca7ca679
Kevin-AC <kevin.argumedo@gmail.com>
2026-07-22 22:00:23 -0500

    Fix: Update viewport meta tag and adjust sidebar padding for safe area

--- personal-finance-app/starter-code/src/App.jsx
+++ personal-finance-app/starter-code/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <main className="w-full h-screen  flex font-public-sans bg-bg overflow-hidden">
+      <main className="w-full h-screen  lg:pb-0 flex font-public-sans bg-bg overflow-hidden pb-[calc(5rem+env(safe-area-inset-bottom))]">
         <Sidebard />
         <section className="flex-1 h-full  overflow-y-auto">
           <Routes>
